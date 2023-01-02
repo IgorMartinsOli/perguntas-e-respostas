930b0efa80086a5a75d5b2b44b17a3fc653cd0fe
igor.martins@smartnx.com <igor.martins@smartnx.com>
2023-01-02 11:57:13 -0300

    finalizando formulario de pergunta

--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT
+app.use(express.static('public'))
 
 app.set('view engine', 'ejs')
 
-app.get('/', (req, res) => {
-    var nome = "Igor";
-    var lang = 'pt-br'
-    res.render('index', {nome, lang})
+app.get('/', (req, res) => res.render('index'))
+
+app.get('/perguntar', (req, res) => {
+    res.render('perguntar')
+})
+
+app.post('/pergunta/salvar', (req, res) => {
+    res.send("Formulario recebido")
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
