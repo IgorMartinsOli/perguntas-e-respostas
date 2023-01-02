33df00e152f94263eb6b5cab706b1060cfb983ff
igor.martins@smartnx.com <igor.martins@smartnx.com>
2023-01-02 12:36:29 -0300

    conexão com o bd

--- index.js
+++ index.js
@@ -2,9 +2,24 @@ const express = require('express')
 const app = express()
 require('dotenv').config()
 const port = process.env.PORT
-app.use(express.static('public'))
+const bodyparser = require('body-parser')
+const connection = require('./database/database')
+const perguntaModel = require('./database/Pergunta')
+
+connection
+    .authenticate()
+    .then(() => {
+        console.log('Conexão com o bd sucesso')
+    })
+    .catch(err => {
+        console.log(`Erro ao conectar com o bd ${err}`)
+    })
 
 app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use(bodyparser.urlencoded({extended: false}))
+app.use(bodyparser.json())
 
 app.get('/', (req, res) => res.render('index'))
 
@@ -13,6 +28,9 @@ app.get('/perguntar', (req, res) => {
 })
 
 app.post('/pergunta/salvar', (req, res) => {
-    res.send("Formulario recebido")
+    const {titulo, descricao} = req.body;
+    res.send(`Formulario recebido!
+    Titulo: ${titulo} 
+    Descricao: ${descricao}`)
 })
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
